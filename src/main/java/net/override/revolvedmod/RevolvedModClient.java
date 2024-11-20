8253a5b27d814dff66a16c51ab9d48733a5b0f75
OverRide-00 <105.OverRide@gmail.com>
2024-11-20 11:23:59 +0100

    Initial commit

---
+++ src/main/java/net/override/revolvedmod/RevolvedModClient.java
@@ -0,0 +1,10 @@
+package net.override.revolvedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RevolvedModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
